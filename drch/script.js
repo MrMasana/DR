a3e67ab7c09f83f62d4e4397953d27a3e2279ff2
MrMasana <84998200+MrMasana@users.noreply.github.com>
2024-04-17 00:22:47 +0300

    Delete drch directory

--- drch/script.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    var menu = document.querySelector('.menu');
-    menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'block' : 'none';
-}
